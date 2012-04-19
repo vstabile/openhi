7e4819ea6b82a5c2f14c060e1b53117eb9b570b0
Victor Stabile <vmstabile@gmail.com>
2012-04-19 15:11:02 -0300

    ready to test

--- README.md
+++ README.md
@@ -8,9 +8,7 @@ Installation
 
 To install using bundler, add OpenHi to your `gemfile` and run `bundle install`:
 
-    gem 'openhi'
-
-To install as a regular gem just type `gem install openhi`
+    gem 'openhi', :git => 'git://github.com/vstabile/openhi.git'
 
 How it works
 ------------
@@ -26,24 +24,23 @@ Use the following code to create a classroom and get it's `room_id` back:
     partner_id = 0       # number
     secret = ''          # string
   
-    openhi = OpenHi::OpenHi.new partner_id, secret
+    openhi = OpenHi::API.new partner_id, secret
 
     course               # number
     level                # number
     lesson               # number
     json                 # string containing json data
 
-    room_id = openhi.create_room course, level, lesson, json
+    room_id = openhi.create_room :course => course, :level => level, :lesson => lesson, :json => json
 
 ### Create tokens
 
 Once you have a `room_id`, you can generate tokens used to join the classroom:
 
-    room_id               # number, returned by openhi.create_
-    room
+    room_id               # number, returned by openhi.create_room
     role                  # string, 'host' or 'attendee'
     user_id               # number, user's identification in your system
     name                  # string, user's name
     skype                 # string, user's skype name if available
 
-    openhi.generate_token room_id, role, user_id, name, skype
+    openhi.generate_token :room_id => room_id, :role => role, :user_id => user_id, :name => name, :skype => skype
--- lib/openhi.rb
+++ lib/openhi.rb
@@ -1,15 +1,19 @@
 require "openhi/version"
+require 'rubygems'
+require 'net/http'
+require 'net/https'
+require 'uri'
+require 'digest/md5'
+require 'cgi'
+require 'openssl'
+require 'base64'
+require 'rexml/document'
+  
+Net::HTTP.version_1_2
+  
+require 'monkey_patches'
 
-module Openhi
-  require 'rubygems'
-  require 'net/http'
-  require 'uri'
-  require 'digest/md5'
-  require 'cgi'
-  require 'openssl'
-  require 'open64'
-  require 'rexml/document'
-
+module OpenHi
   API_URL = "http://localhost/hichinaschool.com/openhi";
 
   DIGEST = OpenSSL::Digest::Digest.new('sha1')
@@ -37,7 +41,7 @@ module Openhi
     ATTENDEE = "attendee"
   end
 
-  class OpenHiSDK
+  class API
     attr_accessor :api_url
   
     @@TOKEN_SALT = "T1=="
@@ -56,16 +60,47 @@ module Openhi
     end
 
     def generate_token(opts = {})
+      { :room_id => '', :role => RoleConstants::ATTENDEE, :user_id => nil, :name => '', :skype => '', :expire_time => nil }.merge!(opts)
+      
+      unless role == RoleConstants::ATTENDEE || role == RoleConstants::HOST
+        raise OpenHiException.new "'#{role}' is not defined as a role"
+      end
 
+      data_params = {
+				:room_id => opts[:room_id],
+        :role => opts[:role],
+        :uid => opts[:user_id],
+        :name => opts[:name],
+        :skype => opts[:skype],
+        :create_time => Time.now.to_i
+      }
+      
+      unless opts[:expire_time].nil?
+				raise OpenHiException.new 'expire_time must be a number' if not opts[:expire_time].is_a?(Numeric)
+        raise OpenHiException.new 'expire_time must be in the future' if opts[:expire_time] < Time.now.to_i
+        raise OpenHiException.new 'expire_time must be in the next 7 days' if opts[:expire_time] > (Time.now.to_i + 604800)
+        data_params[:expire_time] = opts[:expire_time].to_i
+      end
+      
+      data_string = data_params.urlencode
+      
+      sig = sign_string(data_string, @secret)
+      
+      meta_string = {
+				:partner_id => @partner_id,
+				:sig => sig
+      }.urlencode
+      
+      @@TOKEN_SALT + Base64.encode64(meta_string + ":" + data_string).gsub("\n", '')
     end
 
     def create_room(opts = {})
       opts.merge!({:partner_id => @partner_id})
       doc = do_request("/rooms/create.xml", opts)
-      if not doc.get_element('Errors').empty?
+      if not doc.get_elements('Errors').empty?
         raise OpenHiException.new doc.get_elements('Errors')[0].get_elements('error')[0].children[0].to_s
       end
-      OpenHi::Room.new(doc.root.get_elements('Room')[0].get_elements('room_id')[0].children[0].to_s
+      OpenHi::Room.new(doc.root.get_elements('Room')[0].get_elements('room_id')[0].children[0].to_s)
     end
 
     protected
@@ -74,9 +109,33 @@ module Openhi
       OpenSSL::HMAC.hexdigest(DIGEST, secret, data)
     end
 
-    def do_request(api_url, params, token = nil)
+    def do_request(url, params)
+      url = URI.parse(@api_url + url)
+      if params.empty?
+				req = Net::HTTP::Get.new(url.to_s)
+      else
+        req = Net::HTTP::Post.new(url.to_s)
+        req.set_form_data(params)
+      end
 
+			credentials = Base64.encode64("#{@partner_id}:#{@secret}").gsub("\n", '')
+      req.add_field 'Authorization', "Basic #{credentials}"
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true if @api_url.start_with?("https")
+      res = http.start { |http| http.request(req) }
+      case res
+        when Net::HTTPSuccess, Net::HTTPRedirection
+          doc = REXML::Document.new(res.read_body)
+          return doc
+        else
+          res.error!
+        end
+    rescue Net::HTTPExceptions
+      raise
+      raise OpenHiException.new 'HTTP Exceptions'
+    rescue NoMethodError
+      raise
+      raise OpenHiException.new 'No Method Error'
     end
-
   end
 end
